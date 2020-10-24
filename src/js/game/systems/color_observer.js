f958eb119e921d2f1d9a17cd22da0427eb03c707
TcePrepK <56453014+TcePrepK@users.noreply.github.com>
2020-10-24 20:41:42 +0300

    ColorZ-ShrimpEdition Color Observer Bug Fix

--- src/js/game/systems/color_observer.js
+++ src/js/game/systems/color_observer.js
@@ -43,24 +43,35 @@ export class ColorObserverSystem extends GameSystemWithFilter {
         }
     }
 
-    DrawDisplayers(ctx, x, y, pos_x, pos_y, hex, a, align) {
+    DrawDisplayers(ctx, x, y, gen_x, gen_y, hex, a, degree) {
+        let align = Math.radians(degree);
         const color = HexToReadableRGB(hex);
         var text = "";
-        ctx.translate(x + pos_x, y + pos_y);
-        ctx.rotate(align);
+        ctx.translate(x + gen_x, y + gen_y);
         ctx.textAlign = "center";
-
-        if (hex == "aaaaaa") {
+        
+        if (hex == "aa0000" || hex == "0000aa") {
+            ctx.translate(-(x + gen_x), -(y + gen_y));
+            return;
+        } else if (hex == "00aa00") {
+            ctx.textAlign = "center";
+            ctx.rotate(align);
             text = "uncolored";
-            this.drawStroked(ctx, text, 15, 20, 64);  
+            if (degree == 90) {
+                this.drawStroked(ctx, text, 15, -12, 64);  
+            } else if (degree == 270) {
+                this.drawStroked(ctx, text, -16, 20, 64);  
+            } else {
+                this.drawStroked(ctx, text, 15, 20, 64);  
+            }
+            ctx.rotate(-align);
         } else {
-            text = color.slice(4 * a + 1, 4 * a + 4);
             ctx.fillStyle = "#" + hex;    
             ctx.fillRect(0.25, 0.5, 29.25, 29.25);
-            this.drawStroked(ctx, text, 15, 20, 32);    
+            text = color.slice(4 * a + 1, 4 * a + 4); 
+            this.drawStroked(ctx, text, 15, 20, 32);   
         }
-        ctx.rotate(-align);
-        ctx.translate(-(x + pos_x), -(y + pos_y));
+        ctx.translate(-(x + gen_x), -(y + gen_y));
     }
 
     drawStroked(ctx, text, x, y, width) {
@@ -71,6 +82,32 @@ export class ColorObserverSystem extends GameSystemWithFilter {
         ctx.strokeText(text, x, y, width);
         ctx.fillStyle = 'white';
         ctx.fillText(text, x, y, width);
+    }
+
+    drawColorObserverSprite(parameters, entity, color, GeneralX, GeneralY) {
+        let align = entity.components.StaticMapEntity.originalRotation;
+        switch (entity.components.StaticMapEntity.originalRotation) {
+            case 0:
+                this.DrawDisplayers(parameters.context, GeneralX, GeneralY, 1.5, 0, color.slice(0,2) + "0000", 0, 0);
+                this.DrawDisplayers(parameters.context, GeneralX, GeneralY, 32, 0, "00" + color.slice(2,4) + "00", 1, 0);
+                this.DrawDisplayers(parameters.context, GeneralX, GeneralY, 62.5, 0, "0000" + color.slice(4,6), 2, 0);
+                break;
+            case 90:
+                this.DrawDisplayers(parameters.context, GeneralX, GeneralY, 0, 1.5, color.slice(0,2) + "0000", 0, align);
+                this.DrawDisplayers(parameters.context, GeneralX, GeneralY, 0, 32, "00" + color.slice(2,4) + "00", 1, align);
+                this.DrawDisplayers(parameters.context, GeneralX, GeneralY, 0, 62.5, "0000" + color.slice(4,6), 2, align);
+                break;
+            case 180:
+                this.DrawDisplayers(parameters.context, GeneralX, GeneralY, -62.5, 0, color.slice(0,2) + "0000", 0, 0);
+                this.DrawDisplayers(parameters.context, GeneralX, GeneralY, -32, 0, "00" + color.slice(2,4) + "00", 1, 0);
+                this.DrawDisplayers(parameters.context, GeneralX, GeneralY, -1.5, 0, "0000" + color.slice(4,6), 2, 0);
+                break;
+            case 270:
+                this.DrawDisplayers(parameters.context, GeneralX, GeneralY, 0, -1.5, color.slice(0,2) + "0000", 0, align);
+                this.DrawDisplayers(parameters.context, GeneralX, GeneralY, 0, -32, "00" + color.slice(2,4) + "00", 1, align);
+                this.DrawDisplayers(parameters.context, GeneralX, GeneralY, 0, -62.5, "0000" + color.slice(4,6), 2, align);
+                break;
+        }
     }
 
     /**
@@ -102,56 +139,21 @@ export class ColorObserverSystem extends GameSystemWithFilter {
                 if (value.getItemType() === "color") {
                     //console.log(entity.components.StaticMapEntity.originalRotation);
                     const color = value.getAsCopyableKey();
+                    if (!color) {
+                        return;
+                    }
                     const GeneralX = (origin.x + 0.5) * globalConfig.tileSize - 15;
                     const GeneralY = (origin.y + 0.5) * globalConfig.tileSize - 15;
-                    let align = Math.radians(entity.components.StaticMapEntity.originalRotation);
+                    this.drawColorObserverSprite(parameters, entity, color, GeneralX, GeneralY);
 
-                    if (color != "aaaaaa") {
-                        switch (entity.components.StaticMapEntity.originalRotation) {
-                            case 0:
-                                this.DrawDisplayers(parameters.context, GeneralX, GeneralY, 1.5, 0, color.slice(0,2) + "0000", 0, 0);
-                                this.DrawDisplayers(parameters.context, GeneralX, GeneralY, 32, 0, "00" + color.slice(2,4) + "00", 1, 0);
-                                this.DrawDisplayers(parameters.context, GeneralX, GeneralY, 62.5, 0, "0000" + color.slice(4,6), 2, 0);
-                                break;
-                            case 90:
-                                this.DrawDisplayers(parameters.context, GeneralX, GeneralY, 30, 1.5, color.slice(0,2) + "0000", 0, align);
-                                this.DrawDisplayers(parameters.context, GeneralX, GeneralY, 30, 32, "00" + color.slice(2,4) + "00", 1, align);
-                                this.DrawDisplayers(parameters.context, GeneralX, GeneralY, 30, 62.5, "0000" + color.slice(4,6), 2, align);
-                                break;
-                            case 180:
-                                this.DrawDisplayers(parameters.context, GeneralX, GeneralY, -62.5, 0, color.slice(0,2) + "0000", 0, 0);
-                                this.DrawDisplayers(parameters.context, GeneralX, GeneralY, -32, 0, "00" + color.slice(2,4) + "00", 1, 0);
-                                this.DrawDisplayers(parameters.context, GeneralX, GeneralY, -1.5, 0, "0000" + color.slice(4,6), 2, 0);
-                                pinsComp.slots[2].value = COLOR_ITEM_SINGLETONS[color.slice(0,2) + "0000"];
-                                pinsComp.slots[1].value = COLOR_ITEM_SINGLETONS["00" + color.slice(2,4) + "00"];
-                                pinsComp.slots[0].value = COLOR_ITEM_SINGLETONS["0000" + color.slice(4,6)];
-                                break;
-                            case 270:
-                                this.DrawDisplayers(parameters.context, GeneralX, GeneralY, 0, 28.5, color.slice(0,2) + "0000", 0, align);
-                                this.DrawDisplayers(parameters.context, GeneralX, GeneralY, 0, -2, "00" + color.slice(2,4) + "00", 1, align);
-                                this.DrawDisplayers(parameters.context, GeneralX, GeneralY, 0, -32.5, "0000" + color.slice(4,6), 2, align);
-                                break;
-                        }
-                        if (entity.components.StaticMapEntity.originalRotation != 180) {
-                            pinsComp.slots[0].value = COLOR_ITEM_SINGLETONS[color.slice(0,2) + "0000"];
-                            pinsComp.slots[1].value = COLOR_ITEM_SINGLETONS["00" + color.slice(2,4) + "00"];
-                            pinsComp.slots[2].value = COLOR_ITEM_SINGLETONS["0000" + color.slice(4,6)];
-                        }
-                    } else if (color == "aaaaaa") {
-                        switch (entity.components.StaticMapEntity.originalRotation) {
-                            case 0:
-                                this.DrawDisplayers(parameters.context, GeneralX, GeneralY, 32, 0, color, 0, 0);
-                                break;
-                            case 90:
-                                this.DrawDisplayers(parameters.context, GeneralX, GeneralY, 32, 32, color, 0, align);
-                                break;
-                            case 180:
-                                this.DrawDisplayers(parameters.context, GeneralX, GeneralY, -32, 0, color, 0, 0);
-                                break;
-                            case 270:
-                                this.DrawDisplayers(parameters.context, GeneralX, GeneralY, 0, 0, color, 0, align);
-                                break;
-                        }
+                    if (entity.components.StaticMapEntity.originalRotation != 180) {
+                        pinsComp.slots[0].value = COLOR_ITEM_SINGLETONS[color.slice(0,2) + "0000"];
+                        pinsComp.slots[1].value = COLOR_ITEM_SINGLETONS["00" + color.slice(2,4) + "00"];
+                        pinsComp.slots[2].value = COLOR_ITEM_SINGLETONS["0000" + color.slice(4,6)];
+                    } else {
+                        pinsComp.slots[2].value = COLOR_ITEM_SINGLETONS[color.slice(0,2) + "0000"];
+                        pinsComp.slots[1].value = COLOR_ITEM_SINGLETONS["00" + color.slice(2,4) + "00"];
+                        pinsComp.slots[0].value = COLOR_ITEM_SINGLETONS["0000" + color.slice(4,6)];
                     }
                 }
             }
